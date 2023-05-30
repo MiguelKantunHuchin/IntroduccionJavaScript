6c6ca7fe17fc96b313f8024e980979c2f7ad3403
Miguel Kantun <miguelkh21@gmail.com>
2023-05-30 08:49:55 -0600

    Se agrego el condicional Switch; Se agrego informacion y ejemplos de Arrays.

--- controlDeFlujo.js
+++ controlDeFlujo.js
@@ -91,3 +91,70 @@ if (condicion 1) {
 */
 
 
+/*Switch
+
+Esta condicional nos permite hacer multiples operaciones y tomar decisiones en funcion de distintos estados de las variables. 
+Evalua una expresion comparando el valor de un dato o variable, y ejecutando la instruccion asociada a ese caso.
+
+
+ - case: Evalua el caso o condicion asociado que dispara un bloque de codigo
+ - break: Termina el proceso sin que tengamos que pasar por todas las iteraciones o casos.
+ - default: Termina el proceso en caso de que ninguna de las evaluaciones sea la correcta (similar al else)
+
+
+
+switch (expresion o condicion){
+    case valor1:
+        //Codigo que se ejecuta si la condicion es igual a valor1
+    break;
+
+    case valor2:
+        //Codigo que se ejecuta si la condicion es igual a valor2
+    break;
+
+    case valor3:
+        //Codigo que se ejecuta si la condicion es igual a valor3
+    break;
+
+    case valor4:
+        //Codigo que se ejecuta si la condicion es igual a valor4
+    break;
+
+    case valor5:
+        //Codigo que se ejecuta si la condicion es igual a valor5
+    break;
+
+    default:
+        //Ingresaste un piso incorrecto (un bloque de codigo que se ejecuta si no se cumple con ninguna de las condiciones anteriores)
+}
+
+*/
+// Ejemplo de switch
+
+let pisoDestino = prompt("Ingresa un numero de piso del 1 al 5:");
+switch(pisoDestino){
+    case "1":
+    console.log("Vamos al piso 1");
+    break;
+    
+    case "2":
+    console.log("Vamos al piso 2");
+    break;
+
+    case "3":
+    console.log("Vamos al piso 3");
+    break;
+
+    case "4":
+    console.log("Vamos al piso 4");
+    break;
+
+    case "5":
+    console.log("Vamos al piso 5");
+    break;
+
+    default:
+        console.log("Ingresa un piso valido");
+
+    
+}
---
+++ ejercicioZodiaco.js
@@ -0,0 +1,58 @@
+function signoZodiacal(){
+
+let signo= prompt("Cual es tu signo zodiacal? todo en minusculas pls c:");
+
+switch(signo){
+    case "aries":
+        alert("Besto signo, solo ese debería existir");
+        break;
+case "tauro":
+        alert("Alguien le hace honor a su signo");
+        break;
+
+        case "geminis":
+        alert("Ni quien te tope");
+        break;
+
+        case "cancer":
+        alert("Hoy recibiras una gran noticia.... bueno no c:");
+        break;
+
+        case "leo":
+        alert("Hoy para ti es un dia especial");
+        break;
+
+        case "virgo":
+        alert("Encontraras el amor, esperemos");
+        break;
+
+        case "libra":
+        alert("Le caes pesado a todos");
+        break;
+
+        case "escorpio":
+        alert("Tus comentarios le hacen honor a tu signo");
+        break;
+
+        case "capricornio":
+        alert("Otro que le hace honor a su signo jaja");
+        break;
+
+        case "acuario":
+        alert("Aguas con Piscis");
+        break;
+
+        case "piscis":
+        alert("Aguas con Acuario");
+        break;
+
+        case "sagitario":
+        alert("No confies en nadie hoy");
+        break;
+
+        default:
+            alert("Seguro eres Ofiuco");
+
+
+}
+}
